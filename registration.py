3507ebebfad5b5381cbf5bc79db0c77ebffb34de
kheledorogh <Kelehidorogh@yandex.ru>
2024-04-03 17:40:21 +0300

    Add file 3

--- registration.py
+++ registration.py
@@ -7,21 +7,27 @@ class Registration:
         self.code = code
         self.file = file
 
+
+
     def registration(self):
-        with open(self.file) as files:
-            n = json.load(files)
-            count = int(n["count"]) + 1
-            if self.name not in n.keys() and self.code not in n.values():
-                if self.name != '' and self.code != '' and self.name[0] != ' ' and self.code[0] != ' ':
-                    n[self.name] = self.code
+        try:
+            with open(self.file, 'r', encoding='utf-8') as files:
+                n = json.load(files)
+                count = int(n["count"]) + 1
+                if self.name not in n.keys() and self.code not in n.values():
+                    if self.name != '' and self.code != '' and self.name[0] != ' ' and self.code[0] != ' ':
+                        n[self.name] = self.code
+                    else:
+                        raise Exception('Имя или код заняты')
                 else:
-                    print('Код занят')
-            else:
-                print('Код занят')
-            n["count"] = str(count)
-        with open(self.file, 'w+', encoding='utf-8') as files1:
-            print(n)
-            json.dump(n, files1)
+                    raise Exception('Имя или код заняты')
+                n["count"] = str(count)
+            with open(self.file, 'w+', encoding='utf-8') as files1:
+                print(n)
+                json.dump(n, files1)
+        except Exception as e:
+            print(e)
+
 
 
 name = input()
