e9b024a3802bcd6cdc58ccc1d62270d86954a709
kheledorogh <Kelehidorogh@yandex.ru>
2024-04-07 01:20:14 +0300

    Add file 4

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 # from telegram.ext import Application, MessageHandler, filters
 from adminregistration import Admin_registration
 from telegram.ext import *
-from telegram import *
+import telegram
 from telegram import ReplyKeyboardMarkup
 from token_bot import TOKEN
 logging.basicConfig(
@@ -11,19 +11,55 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-reply_keyboard_2 = [['/functions', '/stockmarket'],
-                    ['/settings', '/info_admin']]
 
 reply_keyboard = [['/stockmarket', '/functions'],
                   ['/settings', '/info_admin']]
+reply_keyboard_2 = [['/send_document']]
+reply_keyboard_3 = [['/main_menu']]
+reply_keyboard_4 = [['/to_the_main_menu']]
+reply_keyboard_5 = [['/documentation', '/admin_user'],
+                    ['/back'], ['/to_the_main_menu']]
+
 
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 markup_2 = ReplyKeyboardMarkup(reply_keyboard_2, one_time_keyboard=False)
+markup_3 = ReplyKeyboardMarkup(reply_keyboard_3, one_time_keyboard=True)
+markup_4 = ReplyKeyboardMarkup(reply_keyboard_4, one_time_keyboard=True)
+markup_5 = ReplyKeyboardMarkup(reply_keyboard_5, one_time_keyboard=False)
 
 
-async def v2(update, context):
-    await update.message.reply_text(
-        "Информация об админе.")
+async def start(update, context):
+    await update.message.reply_text("Здравствуйте! Для продолжение работы с ботом нажмите на кнопку ниже",
+                                    reply_markup=markup_3)
+
+
+async def admin_user(update, context):
+    login_admin = context.args[0]
+    password_admin = context.args[1]
+    object_class = Admin_registration(login_admin, password_admin)
+    await update.message.reply_text("📁" if object_class.is_checking_for_admin() else 'Доступ запрещен')
+
+
+async def to_the_main_menu(update, context):
+    await update.message.reply_text('/main_menu')
+
+
+async def back(update, context):
+    await update.message.reply_text('/main_menu')
+
+
+async def documentation(update, context):
+    await update.message.reply_text('📁')
+    await update.message.reply_photo('image1.png')
+
+
+async def main_menu(update, context):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я бот, имеющий возможность работать с данными биржи, и имею дополнительные функции, о которых ты можешь прочитать в пункте настройки документация",
+        reply_markup=markup
+    )
+    await update.message.reply_text("Выберите функцию")
 
 
 async def admin_passwd(update, context):
@@ -32,54 +68,44 @@ async def admin_passwd(update, context):
     object_class = Admin_registration(login_admin, password_admin)
     if object_class.checking_for_admin():
         object_class.registration_admin()
-        await update.message.reply_text("Зарегистрировался! Нажмите на кнопку ниже")
+        await update.message.reply_text("🔐")
+        await update.message.reply_text("Зарегистрировались! Нажмите на кнопку ниже")
         await update.message.reply_text('/start')
     else:
-        await update.message.reply_text("Логин и пароль занят занят")
-
-
-async def v1(update, context):
-    await update.message.reply_text(
-        "")
-
-
-async def start(update, context):
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я бот, имеющий возможность работать с данными биржи, и имею дополнительные\n функции, о которых ты можешь прочитать в пункте настройки документация",
-        reply_markup=markup
-    )
-    await update.message.reply_text("Выберите функцию")
-
+        await update.message.reply_text("❌")
+        await update.message.reply_text("Логин или пароль занят")
 
 
 async def help(update, context):
     await update.message.reply_text(
-        "Я бот справочник.")
+        "документация")
 
 
 async def information_admin(update, context):
     await update.message.reply_text(
-        "Создатель проекта: Рязанов, Роман")
+        "Создатель проекта: Рязанов, Роман", reply_markup=markup_4)
 
 
 async def stockmarket(update, context):
-    await update.message.reply_text("")
+    await update.message.reply_text("1")
 
 
 async def functions(update, context):
-    await update.message.reply_text("")
+    await update.message.reply_text("2")
 
 
 async def settings(update, context):
-    await update.message.reply_text(
-        "",
-        reply_markup=markup_2)
+    await update.message.reply_text('💡', reply_markup=markup_5)
 
 
 def main():
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("back", back))
+    application.add_handler(CommandHandler("admin_user", admin_user))
+    application.add_handler(CommandHandler("documentation", documentation))
+    application.add_handler(CommandHandler("to_the_main_menu", to_the_main_menu))
+    application.add_handler(CommandHandler('main_menu', main_menu))
     application.add_handler(CommandHandler("info_admin", information_admin))
     application.add_handler(CommandHandler("stockmarket", stockmarket))
     application.add_handler(CommandHandler("functions", functions))
